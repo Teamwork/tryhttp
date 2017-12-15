aeba7927af006f219c2257b15f9aec54db93941a
Martin Tournoij <martin@arp242.net>
2017-12-15 14:33:40 +0000

    Add tryhttp
    I only implemented the goroutine scheduler for now, which will be
    sufficient for getdrip and mailchecker, at least for now. I will add a
    RabbitMQ or Redis backend later.

--- tryhttp_test.go
+++ tryhttp_test.go
@@ -1,1 +1,49 @@
 package tryhttp
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/tevino/abool"
+)
+
+func Test(t *testing.T) {
+	c := 0
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if c < 2 {
+			w.WriteHeader(http.StatusTeapot)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		c++
+		w.Write([]byte("hello")) // nolint: errcheck
+	}))
+
+	successCalled := abool.New()
+	h := New(Client{
+		Retry: func(r *http.Request, n int) (time.Duration, bool) {
+			return 200 * time.Millisecond, true
+		},
+		Success: func(r *http.Request, resp *http.Response, attempt int) {
+			successCalled.Set()
+			resp.Body.Close() // nolint: errcheck
+		},
+	})
+	u, _ := url.Parse(server.URL)
+	h.Do(&http.Request{
+		URL: u,
+	})
+
+	time.Sleep(250 * time.Millisecond)
+	if successCalled.IsSet() {
+		t.Fatal("successCalled == true too soon!")
+	}
+
+	Goroutines.Wait()
+	if !successCalled.IsSet() {
+		t.Fatal("successCalled == false")
+	}
+}
